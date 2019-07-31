b2930516c8850fec712e40fba39ea5c089b4751c
Divya Gupta <divya261261@yahoo.com>
2019-08-01 01:23:32 +0530

    Add hosting files

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const serveStatic = require('serve-static');
 const cors = require('cors');
 
 const app = express();
-app.use(serveStatic(`${__dirname}/build`));
+app.use(serveStatic(`${__dirname}/dist`));
 app.use(cors());
 const port = process.env.PORT || 5000;
 app.listen(port);
